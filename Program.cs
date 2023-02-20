4b98ec0e2ee605322768b6dd5d0afdc01fd7120c
Cesar Moreno <cmoreno2@my.wctc.edu>
2023-02-20 14:46:58 -0600

    1.01 now adds new movies to csv when prompted also reads movies from csv

--- Program.cs
+++ Program.cs
@@ -10,24 +10,24 @@ string? resp = Console.ReadLine();
 if (resp == "1")
 {
     StreamWriter sw = new StreamWriter(file, append: true);
-    for (int i = 0; i < 7; i++)
-    {
-        Console.WriteLine("Enter Movie(Y/N)?");
+    // for (int i = 0; i < 7; i++)
+    // {
+    //     Console.WriteLine("Enter Movie(Y/N)?");
         // input the response
-        string NewMovieChoice = Console.ReadLine().ToUpper();
+        // string NewMovieChoice = Console.ReadLine().ToUpper();
         // if the response is anything other than "Y", stop asking
-        if (NewMovieChoice != "Y") { break; }
-        Console.WriteLine("Enter Movie ID");
-        int MovieID = Convert.ToInt32(Console.ReadLine());
+        // if (NewMovieChoice != "Y") { break; }
+            Console.WriteLine("Enter Movie ID");
+            int MovieID = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Give Movie Title");
-        string? MovieTitle = Console.ReadLine();
+            Console.WriteLine("Give Movie Title");
+            string? MovieTitle = Console.ReadLine();
 
-        Console.WriteLine("Give Movie Genre(s)");
-        string? Genre = Console.ReadLine();
-       
-        sw.WriteLine("{0},{1},{2}", MovieID, MovieTitle, Genre);
-    }
+            Console.WriteLine("Give Movie Genre(s)");
+            string? Genre = Console.ReadLine();
+        
+            sw.WriteLine("{0},{1},{2}", MovieID, MovieTitle, Genre);
+    // }
     sw.Close();
 }
 else if (resp == "2")
